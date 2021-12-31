857a9e662b850df9dff70d9cf3e121bc2d90c7d5
ravichadda <ravi.chadda@tcs.com>
2021-12-31 16:06:17 +0530

    c11haooo

--- webpack.config.js
+++ webpack.config.js
@@ -48,7 +48,7 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env','@babel/preset-react'],
+            presets: ['@babel/preset-react', '@babel/preset-env'],
           },
         },
       },
