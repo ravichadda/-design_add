eaf98f6b62a77de0252f727115666abe2c504f96
ravichadda <ravi.chadda@tcs.com>
2021-12-31 16:18:10 +0530

    c11haooooo

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,14 @@
-// Used by jest
-module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-react'],
-  plugins: [['@babel/plugin-transform-runtime']],
-};
+{
+  "presets": [
+    "@babel/preset-env",
+    "@babel/preset-react"
+  ],
+  "plugins": [
+    [
+      "@babel/plugin-proposal-class-properties",
+      {
+        "loose": true
+      }
+    ]
+  ]
+}
